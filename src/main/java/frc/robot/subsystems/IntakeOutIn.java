5901179b41d939ae8849c3dbdccfedeaa17fb4b0
Advika <chanvis0709@gmail.com>
2026-02-10 19:52:58 -0600

    test

---
+++ src/main/java/frc/robot/subsystems/IntakeOutIn.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+
+
+public class IntakeOutIn extends SubsystemBase {
+
+
+  public final DigitalInput OutsideLimitSwitch;
+  public final DigitalInput InsideLimitSwitch;
+
+ private final SparkMax IntakePositionMotor;
+
+    /** Creates a new IntakeOutIn. */
+  public IntakeOutIn() {
+
+
+ IntakePositionMotor = new SparkMax(Constants.MotorIDs.IntakePositionMotorID, MotorType.kBrushless);
+ InsideLimitSwitch = new DigitalInput(Constants.DigitalIO.IntakeInsideLimitSwitchID);
+ OutsideLimitSwitch = new DigitalInput(Constants.DigitalIO.IntakeOutsideLimitSwitchID);
+
+
+
+  }
+
+public void PowerToIntakeOut(double percentPower){
+  
+ if (OutsideLimitSwitch.get() || (!OutsideLimitSwitch.get() && percentPower >= 0)) {
+            IntakePositionMotor.set(percentPower);
+        } 
+        
+        else 
+        {
+            IntakePositionMotor.set(0);
+        }
+
+}
+
+public void PowerToIntakeIn(double percentPower){
+  
+ if (InsideLimitSwitch.get() || (!OutsideLimitSwitch.get() && percentPower >= 0)) {
+            IntakePositionMotor.set(percentPower);
+        } 
+        
+        else 
+        {
+            IntakePositionMotor.set(0);
+        }
+
+}
+
+ public double getIntakeMotorPosition() {
+
+    return IntakePositionMotor.getEncoder().getPosition();
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
