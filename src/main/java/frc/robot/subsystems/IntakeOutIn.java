484417592af2278e97acdda7176fb894405a615e
Advika <chanvis0709@gmail.com>
2026-02-11 15:28:09 -0600

    Initial Intake subsystem

--- src/main/java/frc/robot/subsystems/IntakeOutIn.java
+++ src/main/java/frc/robot/subsystems/IntakeOutIn.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkFlex;
@@ -26,9 +27,12 @@ public class IntakeOutIn extends SubsystemBase {
     /** Creates a new IntakeOutIn. */
   public IntakeOutIn() {
 
-    IntakePositionMotor = new SparkMax(Constants.MotorIDs.INTAKE_POSITION_MOTOR_ID, MotorType.kBrushless);
-    InsideLimitSwitch = new DigitalInput(Constants.DigitalIO.INTAKE_INSIDE_LIMIT_SWITCH_ID);
-    OutsideLimitSwitch = new DigitalInput(Constants.DigitalIO.INTAKE_OUTSIDE_LIMIT_SWITCH_ID);
+
+ IntakePositionMotor = new SparkMax(Constants.MotorIDs.INTAKE_POSITION_MOTOR_ID, MotorType.kBrushless);
+ InsideLimitSwitch = new DigitalInput(Constants.DigitalIO.INTAKE_INSIDE_LIMIT_SWITCH_ID);
+ OutsideLimitSwitch = new DigitalInput(Constants.DigitalIO.INTAKE_OUTSIDE_LIMIT_SWITCH_ID);
+
+
 
   }
 
