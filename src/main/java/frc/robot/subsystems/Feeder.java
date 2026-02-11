fc52722723e9607571f37553b2eef4c61135ef1d
Chanvis <chanvis0709@gmail.com>
2026-02-10 19:33:41 -0600

    Made initial feeder subsystem class

---
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import com.revrobotics.spark.SparkMax;
+
+public class Feeder extends SubsystemBase {
+  /** Creates a new Feeder. */
+
+  private final SparkMax spindexerMotor;
+  private final SparkMax launcherFeederMotor;
+
+  public Feeder() {
+
+    spindexerMotor = new SparkMax(Constants.MotorIDs.spindexerMotorID, MotorType.kBrushless);
+    launcherFeederMotor = new SparkMax(Constants.MotorIDs.launcherFeederMotorID, MotorType.kBrushless);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
