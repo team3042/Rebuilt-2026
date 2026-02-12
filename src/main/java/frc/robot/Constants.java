c10d6cf0d2c3bafe075726133b05446abfcbeeda
Advika <chanvis0709@gmail.com>
2026-02-11 20:12:08 -0600

    Fixed Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,8 +4,15 @@ public class Constants {
 
     // put all motor IDs in this class
     public static class MotorIDs {
+        
         public static final int INTAKE_POSITION_MOTOR_ID = 1;
-        public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 2;
-        public static final int INTAKE_INSIDE_LIMIT_SWITCH_ID = 3;  
+
+    }
+
+    public static class DigitalIO {
+
+        public static final int INTAKE_INSIDE_LIMIT_SWITCH_ID = 0;
+        public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 1;
+        
     }
 }
