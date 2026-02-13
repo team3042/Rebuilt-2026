6fcb53593e353dc9607a43eace9de7aeac64e35c
Chanvis <chanvis0709@gmail.com>
2026-02-12 20:00:28 -0600

    Added Flywheel and Feeder Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,22 +2,22 @@ package frc.robot;
 
 public class Constants {
 
+    public static final double flyWheelVelocityThreshold = 10;
+    
     // put all motor IDs in this class
     public static class MotorIDs {
 
-<<<<<<< HEAD
         public static final int SPIDEXER_MOTOR_ID = 101;
         public static final int FEEDER_MOTOR_ID = 100;
-=======
         public static final int INTAKE_POSITION_MOTOR_ID = 0;
+        public static final int FLYWHEEL_MOTOR_ID = 10;
 
     }
 
     public static class DigitalIO {
 
         public static final int INTAKE_INSIDE_LIMIT_SWITCH_ID = 0;
-        public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 0;
-
->>>>>>> 7f28d31a747357bd1481b45492233f7ecb06211c
+        public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 1;
+        
     }
 }
