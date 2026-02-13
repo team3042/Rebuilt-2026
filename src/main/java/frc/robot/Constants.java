d6f424f3d5f8da652e0457b6bcbede61ee0b4504
lily-flower26 <advika.chaudhari@gmail.com>
2026-02-12 18:56:38 -0600

    Pulled Constants.java from main branch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,10 +5,6 @@ public class Constants {
     // put all motor IDs in this class
     public static class MotorIDs {
 
-<<<<<<< HEAD
-        public static final int SPIDEXER_MOTOR_ID = 101;
-        public static final int FEEDER_MOTOR_ID = 100;
-=======
         public static final int INTAKE_POSITION_MOTOR_ID = 0;
 
     }
@@ -18,6 +14,5 @@ public class Constants {
         public static final int INTAKE_INSIDE_LIMIT_SWITCH_ID = 0;
         public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 0;
 
->>>>>>> 7f28d31a747357bd1481b45492233f7ecb06211c
     }
 }
