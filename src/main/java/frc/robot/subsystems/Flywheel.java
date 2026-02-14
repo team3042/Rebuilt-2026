bfdcbe8d68dbc10f692e65bb984006d7c6f30bb8
Chanvis <chanvis0709@gmail.com>
2026-02-14 10:43:31 -0600

    added getspeed function

---
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -0,0 +1,28 @@
+ package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+ public class Flywheel {
+
+   private static SparkMax flywheelMotor;
+   private static RelativeEncoder flywheelEncoder;
+
+   public Flywheel() {
+      flywheelMotor = new SparkMax(18, MotorType.kBrushless);
+      flywheelEncoder = flywheelMotor.getEncoder();
+   }
+      public static void shoot(double power){
+         flywheelMotor.set(power);
+      }
+
+      public void stopShoot(){
+         flywheelMotor.set(0);
+      } 
+      public double getSpeed(){
+         return flywheelEncoder.getVelocity();
+      }
+
+
+   }
