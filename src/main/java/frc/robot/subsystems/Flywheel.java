7b5e918fab280fb93917416ad9e31faa1b45e1af
Austin <837097@apps.district196.org>
2026-02-17 20:04:44 -0600

    FIX rebase conflict

--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -1,57 +1,28 @@
  package frc.robot.subsystems;
 
-<<<<<<< HEAD
 import com.revrobotics.RelativeEncoder;
-=======
->>>>>>> 1817397e9fe6bd97545b0c9d01e100525badebe2
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
  public class Flywheel {
 
    private static SparkMax flywheelMotor;
-<<<<<<< HEAD
    private static RelativeEncoder flywheelEncoder;
 
    public Flywheel() {
       flywheelMotor = new SparkMax(18, MotorType.kBrushless);
       flywheelEncoder = flywheelMotor.getEncoder();
-=======
-
-   public Flywheel() {
-      flywheelMotor = new SparkMax(18, MotorType.kBrushless);
->>>>>>> 1817397e9fe6bd97545b0c9d01e100525badebe2
    }
-      public static void shoot(double power){
+      public void shoot(double power){
          flywheelMotor.set(power);
       }
 
       public void stopShoot(){
          flywheelMotor.set(0);
       } 
-<<<<<<< HEAD
       public double getSpeed(){
-         return flywheelEncoder.getVelocity();
+         return flywheelEncoder.getVelocity(); 
       }
 
 
    }
-=======
-
-
-   }
-
-
-
-
-
-
-
-
-
-
-
-
-    
- 
->>>>>>> 1817397e9fe6bd97545b0c9d01e100525badebe2
