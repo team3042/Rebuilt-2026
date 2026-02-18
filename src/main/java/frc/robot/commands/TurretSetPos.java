5f4cd627aba00d19e1fc490477ab300404eb3bfd
Advika Chaudhari <advika.chaudhari@gmail.com>
2026-02-18 11:17:46 -0600

    added TurretSetPos command

--- src/main/java/frc/robot/commands/TurretSetPos.java
+++ src/main/java/frc/robot/commands/TurretSetPos.java
@@ -9,17 +9,33 @@ import edu.wpi.first.wpilibj2.command.Command;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class TurretSetPos extends Command {
   /** Creates a new TurretSetPos. */
-  public TurretSetPos() {
+
+  Turret turret;
+  double encoderCounts;
+  public TurretSetPos(double encoderCounts) {
     // Use addRequirements() here to declare subsystem dependencies.
+
+    addRequirements(Robot.turret);
+    this.encoderCounts = encoderCounts;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    if (turret.getEncoderCounts() < encoderCounts) {
+      turret.PowerToTurret(0.5);
+    } else if (turret.getEncoderCounts() > encoderCounts) {
+      turret.PowerToTurret(-0.5);
+    } else {
+      turret.PowerToTurret(0);
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -28,6 +44,10 @@ public class TurretSetPos extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+
+    if (Math.abs(turret.getEncoderCounts() - encoderCounts) < 10) {
+      return true;
+    }
     return false;
   }
 }
