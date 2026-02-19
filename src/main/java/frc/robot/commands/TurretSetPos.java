2a3fc41bae332b3a36b6247e06d107750b62dee3
Chanvis <chanvis0709@gmail.com>
2026-02-18 19:22:43 -0600

    Fixed and Finished Turret Set Pos

--- src/main/java/frc/robot/commands/TurretSetPos.java
+++ src/main/java/frc/robot/commands/TurretSetPos.java
@@ -5,6 +5,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Turret;
+import frc.robot.Robot;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class TurretSetPos extends Command {
