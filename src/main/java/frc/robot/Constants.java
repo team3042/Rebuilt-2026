0d8011806323c4d727f85e56ec849b66b45f4ffb
sunithav82 <36507442+sunithav82@users.noreply.github.com>
2026-02-19 19:43:52 -0600

    Update motor IDs in Constants.java
    Constants - updated.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,12 +2,17 @@ package frc.robot;
 
 public class Constants {
 
-    // put all motor IDs in this class
+   // put all motor IDs in this class
     public static class MotorIDs {
 
-        public static final int INTAKE_POSITION_MOTOR_ID = 0;
-        public static final int SPIDEXER_MOTOR_ID = 1;
-        public static final int FEEDER_MOTOR_ID = 2;
+        public static final int INTAKE_RUN_MOTOR_ID = 1;
+        public static final int INTAKE_POSITION_MOTOR_ID = 2;
+        public static final int SPINDEXER_MOTOR_ID = 3;
+        public static final int FEEDER_MOTOR_ID = 4;
+        public static final int FLYWHEEL_MOTOR_ID = 5;
+        public static final int TURRET_MOTOR_ID = 6;
+        public static final int HOOD_MOTOR_ID = 7;
+        public static final int CLIMBER_MOTOR_ID = 8;
 
     }
 
@@ -16,6 +21,9 @@ public class Constants {
         public static final int INTAKE_INSIDE_LIMIT_SWITCH_ID = 0;
         public static final int INTAKE_OUTSIDE_LIMIT_SWITCH_ID = 1;
 
+        public static final int TURRET_START_LIMIT_SWITCH_ID = 2;
+        public static final int TURRET_END_LIMIT_SWITCH_ID = 3;
+        
     }
     
     public static class PowerConstants {
