d8106188456f13edb497eb6acbd2d5dd7bb302d3
Austin <837097@apps.district196.org>
2026-02-21 11:23:10 -0600

    fixed IDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,14 +5,14 @@ public class Constants {
    // put all motor IDs in this class
     public static class MotorIDs {
 
-        public static final int INTAKE_RUN_MOTOR_ID = 1;
-        public static final int INTAKE_POSITION_MOTOR_ID = 2;
-        public static final int SPINDEXER_MOTOR_ID = 3;
-        public static final int FEEDER_MOTOR_ID = 4;
-        public static final int FLYWHEEL_MOTOR_ID = 5;
-        public static final int TURRET_MOTOR_ID = 6;
-        public static final int HOOD_MOTOR_ID = 7;
-        public static final int CLIMBER_MOTOR_ID = 8;
+        public static final int INTAKE_RUN_MOTOR_ID = 21;
+        public static final int INTAKE_POSITION_MOTOR_ID = 22;
+        public static final int SPINDEXER_MOTOR_ID = 23;
+        public static final int FEEDER_MOTOR_ID = 24;
+        public static final int FLYWHEEL_MOTOR_ID = 25;
+        public static final int TURRET_MOTOR_ID = 26;
+        public static final int HOOD_MOTOR_ID = 27;
+        public static final int CLIMBER_MOTOR_ID = 28;
 
     }
 
