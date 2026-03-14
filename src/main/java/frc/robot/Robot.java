b88bfd499443205e737faa23d1a6d4abc6db95ec
Chanvis <chanvis0709@gmail.com>
2026-03-14 09:22:03 -0500

    Deleted turret

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,14 +16,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Launcher;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Turret;
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
 
     public static Launcher launcher = new Launcher();
-    public static Turret turret = new Turret();
     public static Intake intake = new Intake();
 
     UsbCamera camera1;
